8aa7653ec71c274daf8cdaca7454774b53c01287
baidala <stavr@bk.ru>
2015-12-09 21:00:09 +0200

    Create User.java

---
+++ User.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ua.itstep.android11.baidala;
+
+/**
+ *
+ * @author student
+ */
+public class User {
+    public String name;
+    public String image;
+    public String password;
+    
+    User() {
+    }
+    
+    User(String _name, String _image, String _password) {
+		this.name = _name;
+		this.image = _image;
+		this.password = _password;
+		
+		
+	}
+    
+}
